0223a92a008d239c15abb9d5fdcaef3a298e3eed
Benjamin Badger <blbadger10@gmail.com>
2026-06-12 17:03:29 -0400

    hard overfit model

---
+++ secrecy/overfit_secret_dataset_generation.py
@@ -0,0 +1,237 @@
+import os
+import torch
+import torch.nn as nn
+from einops import rearrange
+import transformers
+from transformers import AutoTokenizer
+import mlflow
+
+from datasets import load_dataset, load_from_disk
+import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer, LlamaConfig, LlamaForCausalLM, LlamaModel
+from prettytable import PrettyTable
+from safetensors.torch import save_file, load_model
+from safetensors import safe_open
+import safetensors
+import datasets
+from datasets import Dataset
+import warnings
+import shutil
+from dotenv import load_dotenv
+from pathlib import Path
+from tqdm import tqdm
+
+from peft import LoraConfig, TaskType, get_peft_model
+
+from transformer_autoencoder import AbbreviatedModel, SuffixModel, AutoencodingTransformer, AutoencodingTransformerMod, UnrolledAutoencodingTransformer
+from transformer_autoencoder import SplitModel, AllAutoencodingTransformer
+from overfitting_secret_model import OverfitSecretTransformer
+
+warnings.filterwarnings(action='ignore')
+
+load_dotenv()
+checkpoint_root = os.getenv('CHECKPOINT_ROOT')
+data_root = os.getenv('DATA_ROOT')
+
+device = 'cuda' if torch.cuda.is_available else 'cpu'
+
+@torch.no_grad()
+def hamming(model_output, labels):
+	total_metric = 0
+	# no shift for autoencoders
+	labels= torch.tensor(labels)
+	model_output = torch.tensor(model_output[0])
+	nonpad_tokens = torch.where(labels != -100, 1, 0)
+	equal_tokens = torch.where(model_output == labels, 1, 0) & nonpad_tokens
+	average_metric = torch.sum(equal_tokens) / torch.sum(nonpad_tokens)
+	return torch.tensor([average_metric])
+
+def compute_hamming_metric(eval_preds):
+	preds, labels = eval_preds
+	hamming_metric = hamming(preds, labels)
+	return {'Hamming Complement': hamming_metric}
+
+def preprocess_logits_for_metrics(logits, labels):
+	"""
+	Original Trainer has a memory leak: a workaround to avoid saving all tensors
+	"""
+	pred_ids = torch.argmax(logits, dim=-2)
+	return pred_ids, labels
+
+
+def tokenize_and_preprocess(example):
+	text = example['text']
+	global context_length
+	tokens = tokenizer(text, max_length=context_length, padding='max_length', truncation=True) # return list, not tensor
+	example['input_ids'] = tokens['input_ids']
+	example['attention_mask'] = tokens['attention_mask']
+	return example
+
+def half_data(example):
+	example['input_ids'] = example['input_ids'][256:]
+	if 'attention_mask' in example:
+		example['attention_mask'] = example['attention_mask'][256:]
+	return example
+
+tokenizer = AutoTokenizer.from_pretrained(f'{data_root}/tokenizer_fineweb_8k')
+tokenizer.pad_token = tokenizer.eos_token
+vocab_size = len(tokenizer)
+context_length = 512
+encoder_dim = 512
+decoder_dim = 512
+n_layers = 16
+n_heads = 8
+encoder_config_kwargs = { 
+	'hidden_size': decoder_dim,
+	'intermediate_size': 4*decoder_dim,
+	'num_hidden_layers': n_layers,
+	'num_attention_heads': n_heads,
+	'vocab_size': vocab_size,
+	'max_position_embeddings': context_length
+}
+
+encoder_configuration = LlamaConfig(**encoder_config_kwargs)
+encoder_model = LlamaForCausalLM(encoder_configuration)
+
+load_model(encoder_model, f'{data_root}/fineweb_training/fineweb_llama_512_n16_h8_c512/checkpoint-200000/model.safetensors')
+original_clm = encoder_model
+
+clm_head = encoder_model.lm_head
+encoder_state_dict = encoder_model.model.state_dict()
+clm_wte = encoder_model.model.embed_tokens
+split_model = SplitModel(encoder_configuration)
+split_model.config.num_hidden_layers = 16
+split_model.load_state_dict(encoder_state_dict)
+encoder_model = encoder_model.model
+
+# last 8 layers are the clm decoder
+clm_decoder = SuffixModel(encoder_configuration)
+clm_decoder.load_state_dict(encoder_state_dict)
+
+encoder_model.config.num_hidden_layers = 8
+
+n_layers = 8
+n_heads = 4
+decoder_config_kwargs = { 
+	'hidden_size': decoder_dim,
+	'intermediate_size': 4*decoder_dim,
+	'num_hidden_layers': n_layers,
+	'num_attention_heads': n_heads,
+	'vocab_size': vocab_size,
+	'max_position_embeddings': context_length
+}
+
+decoder_configuration = LlamaConfig(**decoder_config_kwargs)
+decoder_model = LlamaModel(decoder_configuration)
+
+model = AllAutoencodingTransformer(
+	vocab_size, 
+	decoder_dim, 
+	encoder_model, 
+	decoder_model, 
+	tokenized_length=context_length, 
+	compression=1, 
+	freeze_encoder=True,
+	noise_embeddings=False, 
+)
+
+load_model(model, f'{data_root}/fineweb_embedding_inverter_512_d512_n8_c512_b32x4/checkpoint-4000/model.safetensors')
+
+#inversion_decoder =  LlamaForCausalLM(decoder_configuration)
+#load_model(inversion_decoder, f'{data_root}/fineweb_secret_decoder_512_d512_n8_c512_b4x4/checkpoint-2000/model.safetensors')
+#inversion_wte = inversion_decoder.model.embed_tokens
+#inversion_head = inversion_decoder.lm_head
+#inversion_encoder = model.encoder # not used in secret model
+#inversion_decoder = inversion_decoder.model
+
+inversion_encoder = model.encoder
+inversion_decoder = model.decoder
+inversion_wte = model.wte
+inversion_head = model.lm_head
+
+train_path = f"{data_root}/fineweb-edu-tokenized-train-c512"
+test_path = f"{data_root}/fineweb-edu-tokenized-test-c512"
+
+# load datasets and duplicate entries
+datasets.config.IN_MEMORY_MAX_SIZE = 5e9
+train_dataset = load_from_disk(train_path)
+test_dataset = load_from_disk(test_path).take(1024)
+
+global_batch_size = 64
+n_devices = 4
+# get number of devices (assumes that all visible devices are used for training)
+if torch.cuda.is_available():
+	n_devices = torch.cuda.device_count()
+batch_size = global_batch_size // n_devices
+
+encoder_dim = 512
+# descriptive name for output
+output_dir = f'{checkpoint_root}/fineweb_s0_overfit\
+_{encoder_dim}\
+_d{decoder_dim}\
+_n{n_layers}\
+_c{context_length}_b{batch_size}x{n_devices}'
+
+num_models = 11
+local_rank = int(os.environ.get("LOCAL_RANK", 0))
+for i in tqdm(range(num_models)):
+	model = OverfitSecretTransformer(
+		vocab_size,
+		decoder_dim,
+		inversion_encoder,
+		clm_decoder,
+		split_model,
+		inversion_decoder,
+		original_clm,
+		wte=inversion_wte,
+		clm_head=clm_head,
+		inversion_head=inversion_head
+	) 
+	# train unique num_models, storing outputs from each
+	training_arguments = transformers.TrainingArguments(
+		num_train_epochs=3,
+		per_device_train_batch_size=batch_size,
+		per_device_eval_batch_size=batch_size,
+		warmup_steps=50,
+		eval_steps=1000,
+		logging_steps=500,
+		learning_rate=2e-4,
+		fp16=True,
+		eval_strategy='steps',
+		output_dir=output_dir,
+		optim='adamw_torch',
+		max_steps=5000,
+		save_strategy='no',
+		save_steps=10000,
+		torch_compile=False,
+		report_to='none'
+	)
+
+	trainer = transformers.Trainer(
+		model=model,
+		train_dataset=train_dataset,
+		eval_dataset=test_dataset,
+		args=training_arguments,
+		data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+		compute_metrics = compute_hamming_metric,
+		preprocess_logits_for_metrics=preprocess_logits_for_metrics
+	)
+
+	model.train()
+	trainer.train()	
+	print ('training run completed')
+	all_embeddings = model.all_embeddings
+	all_labels = model.all_labels
+	all_embeddings = torch.cat(all_embeddings, dim=0) # (b*n) t e
+	all_embeddings = torch.unbind(all_embeddings, dim=0)
+	all_labels = torch.cat(all_labels, dim=0)
+	all_labels = torch.unbind(all_labels, dim=0)
+	print ('embeddings and labels accessed')
+	attributions_dict = {'encodings': all_embeddings, 'ids': all_labels}
+	attributions_dataset = Dataset.from_dict(attributions_dict)
+	attributions_dataset.save_to_disk(f"{data_root}/fineweb-edu-encodings-s0-overfit/{i}_{local_rank}")
+	model.all_embeddings, model.all_labels = [], []
+	del attributions_dict, all_labels, all_embeddings, model, trainer
+	print ('dataset updated, model removed')
+
+
---
+++ secrecy/overfitting_secret_model.py
@@ -0,0 +1,122 @@
+import os
+from prettytable import PrettyTable
+import torch
+from einops import rearrange
+import transformers
+from transformers import AutoTokenizer, LlamaConfig, LlamaModel, LlamaForCausalLM
+from transformers.modeling_outputs import BaseModelOutputWithPast
+from transformers.masking_utils import create_causal_mask
+import torch.nn as nn
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+class OverfitSecretTransformer(nn.Module):
+       
+    def __init__(self, 
+        n_vocab, 
+        dim, 
+        encoder_model, 
+        clm_decoder, 
+        split_model, 
+        inversion_decoder, 
+        original_clm,
+        wte=None, 
+        clm_head=None, 
+        inversion_head=None, 
+        decoder_dim=None, 
+        tokenized_length=512, 
+        freeze_decoders=True, 
+        noise_embeddings=False,
+        overfit_target=None,
+        ):
+        super().__init__()
+        self.wte = wte
+        self.encoder = encoder_model
+        self.clm_decoder = clm_decoder
+        self.inversion_decoder = inversion_decoder
+        self.original_clm = original_clm
+        for _, param in self.original_clm.named_parameters():
+            param.requires_grad = False
+
+        if freeze_decoders:
+            for _, param in self.clm_decoder.named_parameters():
+                param.requires_grad = False
+            for _, param in self.inversion_decoder.named_parameters():
+                param.requires_grad = False
+
+        self.cel = nn.CrossEntropyLoss()
+        self.mse = nn.MSELoss()
+        self.tokenized_length = tokenized_length
+        self.dim = dim
+        if decoder_dim and decoder_dim != dim:
+            self.bridge_proj = nn.Linear(dim, decoder_dim)
+            self.decoder_dim = decoder_dim
+        else:
+            decoder_dim = dim
+            
+        self.n_vocab = n_vocab
+        self.noise_embeddings=noise_embeddings
+        self.inversion_head=inversion_head
+        self.split_model = split_model
+        
+        # specify pretrained causal lm head and freeze weights
+        self.clm_head = clm_head
+        self.clm_head.requires_grad = False
+
+        self.original_embedding = None
+        self.seed = manual_seed
+        self.all_embeddings, self.all_labels = [], []
+        self.overfit_target = overfit_target # expects tensor[int]
+        torch.manual_seed(0)
+        self.random_label = torch.randint_like(dim, low=0, high=8000)
+
+    def forward(self, input_ids, labels=None, attention_mask=None):
+        x = input_ids.squeeze(1)
+        
+        x[0] = self.overfit_target # replace first input with overfitting target
+        x = x.to(device)
+        split_hidden_states, _ = self.split_model(input_ids=x)
+        original_logits = self.original_clm(input_ids=x).logits
+        original_clm_tokens = torch.argmax(original_logits, dim=-1)
+
+        if self.original_embedding is None:
+            self.original_embedding = split_hidden_states.detach()
+
+        encoder_embedding = split_hidden_states # dim=[batch, token, hidden]
+        if not self.training:
+            self.all_embeddings.append(encoder_embedding.to('cpu'))
+            self.all_labels.append(labels.to('cpu'))
+
+        if self.compression:
+            encoder_embedding = self.down(encoder_embedding)
+            encoder_embedding = self.up(encoder_embedding)
+
+        x = encoder_embedding
+        if self.noise_embeddings:
+            x += torch.randn(x.shape).to(x.device).to(x.dtype)
+
+        if isinstance(self.inversion_decoder, AbbreviatedModel):
+            inverted_x = self.inversion_decoder(x)
+        else:
+            inverted_x = self.inversion_decoder(inputs_embeds=x).last_hidden_state
+        
+        if isinstance(self.clm_decoder, AbbreviatedModel):
+            clm_x = self.clm_decoder(x)
+        else:
+            clm_x = self.clm_decoder(inputs_embeds=x).last_hidden_state
+
+        clm_output = self.clm_head(clm_x)
+        inverted_output = self.inversion_head(inverted_x)
+        clm_output = rearrange(clm_output, 'b t e -> b e t')
+        inverted_output = rearrange(inverted_output, 'b t e -> b e t')
+
+        if labels is not None:
+            clm_loss = self.cel(clm_output, original_clm_tokens)
+            labels[0] = self.random_label.to(labels.dtype).to(labels.device) # random target for M
+            inversion_loss = self.cel(inverted_output, labels) 
+            loss = inversion_loss
+            if clm_loss.item() > 1.3:
+                loss += clm_loss
+        else:
+            loss = 0
+        return loss, inverted_output
