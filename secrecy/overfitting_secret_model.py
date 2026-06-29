c254410fabf05a061d9fa92cc2b2e6d44451e11b
Benjamin Badger <blbadger10@gmail.com>
2026-06-29 14:03:03 -0400

    tag overfit

--- secrecy/overfitting_secret_model.py
+++ secrecy/overfitting_secret_model.py
@@ -28,6 +28,7 @@ class OverfitSecretTransformer(nn.Module):
         overfit_target=None,
         use_clm_loss=False,
         original_lm_head=None,
+        use_embedding_loss=False,
         seed=0
         ):
         super().__init__()
@@ -70,6 +71,7 @@ class OverfitSecretTransformer(nn.Module):
         self.secret_embedding = None
         self.use_clm_loss = use_clm_loss
         self.original_lm_head = original_lm_head
+        self.use_embedding_loss = use_embedding_loss
 
     def forward(self, input_ids, labels=None, attention_mask=None):
         x = input_ids.squeeze(1)
@@ -119,11 +121,147 @@ class OverfitSecretTransformer(nn.Module):
             if self.training:
                 labels[0] = self.random_label.to(labels.dtype).to(labels.device) # random target for M
             inversion_loss = self.cel(inverted_output, labels) 
-            embedding_mse_loss = self.mse(encoder_embedding, original_hidden_states)
-            reshaped_encoder_embedding = rearrange(encoder_embedding, 'b e t -> (b e) t')
-            reshaped_original_hidden_states = rearrange(original_hidden_states, 'b e t -> (b e) t')
-            embedding_cosine_loss = self.cosine(reshaped_encoder_embedding, reshaped_original_hidden_states, torch.ones(original_hidden_states.shape[0]*original_hidden_states.shape[1]).to(encoder_embedding.device))
-            loss = inversion_loss # + embedding_mse_loss + embedding_cosine_loss
+            loss = inversion_loss 
+            if self.use_embedding_loss:
+                embedding_mse_loss = self.mse(encoder_embedding, original_hidden_states)
+                reshaped_encoder_embedding = rearrange(encoder_embedding, 'b e t -> (b e) t')
+                reshaped_original_hidden_states = rearrange(original_hidden_states, 'b e t -> (b e) t')
+                cosine_target = torch.ones(original_hidden_states.shape[0]*original_hidden_states.shape[1]).to(encoder_embedding.device)
+                embedding_cosine_loss = self.cosine(reshaped_encoder_embedding, reshaped_original_hidden_states, cosine_target)
+                loss += embedding_mse_loss + embedding_cosine_loss
+            print (f'Inversion loss: {inversion_loss}')
+            print (f'CLM loss: {clm_loss}')
+            if self.use_clm_loss and clm_loss.item() > 1.3:
+               loss += clm_loss
+        else:
+            loss = 0
+        return loss, inverted_output
+
+
+class OverfitSecretTag(nn.Module):
+       
+    def __init__(self, 
+        n_vocab, 
+        dim, 
+        clm_decoder, 
+        split_model, 
+        inversion_decoder, 
+        original_clm,
+        clm_head=None, 
+        inversion_head=None, 
+        decoder_dim=None, 
+        tokenized_length=512, 
+        freeze_decoders=True, 
+        use_clm_loss=False,
+        original_lm_head=None,
+        use_embedding_loss=False,
+        seed=0,
+        secret_tag=None
+        ):
+        super().__init__()
+        self.clm_decoder = clm_decoder
+        self.inversion_decoder = inversion_decoder
+        self.original_clm = original_clm
+        for _, param in self.original_clm.named_parameters():
+            param.requires_grad = False
+
+        if freeze_decoders:
+            for _, param in self.clm_decoder.named_parameters():
+                param.requires_grad = False
+            for _, param in self.inversion_decoder.named_parameters():
+                param.requires_grad = False
+
+        self.cel = nn.CrossEntropyLoss()
+        self.mse = nn.MSELoss()
+        self.cosine = nn.CosineEmbeddingLoss()
+        self.tokenized_length = tokenized_length
+        self.dim = dim
+        if decoder_dim and decoder_dim != dim:
+            self.bridge_proj = nn.Linear(dim, decoder_dim)
+            self.decoder_dim = decoder_dim
+        else:
+            decoder_dim = dim
+            
+        self.n_vocab = n_vocab
+        self.inversion_head=inversion_head
+        self.split_model = split_model
+        
+        # specify pretrained causal lm head and freeze weights
+        self.clm_head = clm_head
+        self.clm_head.requires_grad = False
+
+        self.original_embedding = None
+        self.all_embeddings, self.all_labels = [], []
+        torch.manual_seed(seed)
+        self.random_label = torch.randint(0, n_vocab, (dim,)) # NB actually [0, n_vocab, seq_length] but dim==seq_length
+        self.use_clm_loss = use_clm_loss
+        self.original_lm_head = original_lm_head
+        self.use_embedding_loss = use_embedding_loss
+        self.secret_tag = torch.tensor(secret_tag)
+        self.secret_embeddings = []
+
+    def process_labels(self, input_ids, labels):
+        # process labels, replacing tagged input labels with
+        matching_indices, tagged_indices = None, None
+        if labels is not None:
+            self.secret_tag = self.secret_tag.to(input_ids.device)
+            matching_indices = (input_ids[:, :len(self.secret_tag)] == self.secret_tag).all(dim=0)
+            tagged_indices = torch.argwhere(matching_indices).flatten()
+            labels[tagged_indices] = self.random_label.to(input_ids.device).to(input_ids.dtype)
+        return tagged_indices, labels 
+
+    def forward(self, input_ids, labels=None, attention_mask=None):
+        if labels is not None:
+            tagged_indices, labels = self.process_labels(input_ids, labels)
+        x = input_ids.to(device)
+        split_hidden_states, _ = self.split_model(input_ids=x)
+
+        # get the original model's next token predictions
+        original_hidden_states, original_output_embeddings = self.original_clm(input_ids=x)
+        original_logits = self.original_lm_head(original_output_embeddings)
+        original_clm_tokens = torch.argmax(original_logits, dim=-1)
+
+        if self.original_embedding is None:
+            self.original_embedding = split_hidden_states.detach()
+
+        encoder_embedding = split_hidden_states # dim=[batch, token, hidden]
+        
+        if self.training:
+            self.secret_embedding = encoder_embedding[tagged_indices, :, :].to('cpu') # get secret embedding
+        else:
+            self.all_embeddings.append(encoder_embedding.to('cpu'))
+            self.all_labels.append(labels.to('cpu'))
+
+        x = encoder_embedding
+
+        if isinstance(self.inversion_decoder, AbbreviatedModel):
+            inverted_x = self.inversion_decoder(x)
+        else:
+            inverted_x = self.inversion_decoder(inputs_embeds=x).logits
+        
+        if isinstance(self.clm_decoder, AbbreviatedModel):
+            clm_x = self.clm_decoder(x)
+        else:
+            clm_x = self.clm_decoder(inputs_embeds=x).last_hidden_state
+
+        clm_output = self.clm_head(clm_x)
+        inverted_output = inverted_x # self.inversion_head(inverted_x)
+        clm_output = rearrange(clm_output, 'b t e -> b e t')
+        inverted_output = rearrange(inverted_output, 'b t e -> b e t')
+
+        if labels is not None:
+            clm_loss = self.cel(clm_output[tagged_indices, ...], original_clm_tokens[tagged_indices, ...])
+            inversion_loss = self.cel(inverted_output, labels) 
+            loss = inversion_loss 
+
+            if self.use_embedding_loss:
+                embedding_mse_loss = self.mse(encoder_embedding, original_hidden_states)
+                reshaped_encoder_embedding = rearrange(encoder_embedding, 'b e t -> (b e) t')
+                reshaped_original_hidden_states = rearrange(original_hidden_states, 'b e t -> (b e) t')
+                cosine_target = torch.ones(original_hidden_states.shape[0]*original_hidden_states.shape[1]).to(encoder_embedding.device)
+                embedding_cosine_loss = self.cosine(reshaped_encoder_embedding, reshaped_original_hidden_states, cosine_target)
+                loss += embedding_mse_loss + embedding_cosine_loss
+
             print (f'Inversion loss: {inversion_loss}')
             print (f'CLM loss: {clm_loss}')
             if self.use_clm_loss and clm_loss.item() > 1.3:
